0ec83da01b06977a62565213331f51ca7b91d7c1
senthil kumar <senthilthoondi@gmail.com>
2023-05-07 11:59:40 +0530

    cors and posts crud

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,56 @@
+class Api::PostsController < ApplicationController
+  before_action :set_post, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: api_post_url(@post)
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+  end
+
+  def render_404
+    render json: { error: "Not found", status: 404 }
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
